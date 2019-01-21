1169cd6bfa220b5d15a7ea4907c92721afe49db6
Christoph Sonntag <info@chrisonntag.com>
2019-01-21 15:00:17 +0100

    Create peewee models

--- database.py
+++ database.py
@@ -1,3 +1,32 @@
+from peewee import *
+import datetime
+
+
+db = SqliteDatabase('heatmap_database.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    username = CharField(unique=True)
+    key = UUIDField(unique=True)
+    points = IntegerField()
+    rank = IntegerField()
+
+
+class DayAction(BaseModel):
+    user = ForeignKeyField(User, backref='actions')
+    actions = IntegerField()
+    date = DateTimeField(default=datetime.datetime.now)
+
+
+def create_tables():
+    with db:
+        db.create_tables([User, DayAction])
+
 
 
 def get_user(username):
