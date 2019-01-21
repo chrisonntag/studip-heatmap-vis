96481358a2bc9a78a5ec95c88340da3d47cda34e
Christoph Sonntag <info@chrisonntag.com>
2019-01-21 17:14:15 +0100

    Add functions for creating user and dayactions

--- database.py
+++ database.py
@@ -2,36 +2,67 @@ from peewee import *
 import datetime
 
 
-db = SqliteDatabase('heatmap_database.db')
+DATABASE = SqliteDatabase('heatmap_database.db')
 
 
 class BaseModel(Model):
     class Meta:
-        database = db
+        database = DATABASE
 
 
 class User(BaseModel):
     username = CharField(unique=True)
     key = UUIDField(unique=True)
-    points = IntegerField()
-    rank = IntegerField()
+    points = IntegerField(null=True)
+    rank = IntegerField(null=True)
 
 
 class DayAction(BaseModel):
     user = ForeignKeyField(User, backref='actions')
     actions = IntegerField()
-    date = DateTimeField(default=datetime.datetime.now)
+    date = DateField(default=datetime.date.today())
 
 
 def create_tables():
-    with db:
-        db.create_tables([User, DayAction])
+    with DATABASE:
+        DATABASE.create_tables([User, DayAction])
 
 
+def user_exists(username, key):
+    try:
+        user = User.get(User.username == username, User.key == key)
+    except User.DoesNotExist:
+        return False
 
-def get_user(username):
-    return None
-
-def register(username, key):
     return True
+
+
+def create_user(username, key, points, rank):
+    try:
+        with DATABASE.atomic():
+            # Attempt to create the user. If the username is taken, due to the
+            # unique constraint, the database will raise an IntegrityError.
+            user = User.create(
+                username=username,
+                key=key,
+                points=points,
+                rank=rank)
+    except IntegrityError:
+        raise IntegrityError
+
+
+def create_dayaction(username, actions):
+    try:
+        with DATABASE.atomic():
+            # Attempt to create the user. If the username is taken, due to the
+            # unique constraint, the database will raise an IntegrityError.
+            user = User.get(User.username == username)
+            dayaction = DayAction.create(
+                user=user,
+                actions=actions)
+    except User.DoesNotExist:
+        return False
+
+    return True
+
 
