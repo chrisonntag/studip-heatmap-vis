6292601bc0286f0c248ae93c7428fd37021264bd
Christoph Sonntag <info@chrisonntag.com>
2019-01-21 17:14:28 +0100

    Add dummy functions

--- studip.py
+++ studip.py
@@ -1,5 +1,16 @@
+import os
 
 def is_valid(username):
     # TODO: Implement
     return True
+
+
+def get_points(username):
+    # TODO: Implement
+    return 100000
+
+
+def get_rank(username):
+    # TODO: Implement
+    return 1
 
